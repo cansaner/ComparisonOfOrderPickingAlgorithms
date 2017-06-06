2dfe2f371afc772294f30ac6d2c909194fdba45f
Saner, Can <can.saner@accenture.com>
2017-06-06 16:03:13 +0300

    - added some new tryout code to manage Item List in a problem.

---
+++ Block.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ComparisonOfOrderPickingAlgorithms
+{
+    public class Block
+    {
+        private Coordinate location;
+
+        public Coordinate Location
+        {
+            get
+            {
+                return location;
+            }
+            set
+            {
+                location = value;
+            }
+        }
+
+        public int AInfo
+        {
+            get
+            {
+                return location.Y;
+            }
+            set
+            {
+                location.Y = value;
+            }
+        }
+
+        private StorageUnit[] storageUnits;
+
+        public StorageUnit[] StorageUnits
+        {
+            get
+            {
+                return storageUnits;
+            }
+            set
+            {
+                storageUnits = value;
+            }
+        }
+
+        public Block(int aInfo, int numberOfStorageUnits)
+        {
+            this.location = new Coordinate(0, aInfo);
+            this.storageUnits = new StorageUnit[numberOfStorageUnits];
+        }
+    }
+}
---
+++ StorageUnit.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ComparisonOfOrderPickingAlgorithms
+{
+    public class StorageUnit
+    {
+        private Coordinate location;
+
+        public Coordinate Location
+        {
+            get
+            {
+                return location;
+            }
+            set
+            {
+                location = value;
+            }
+        }
+
+        public int AInfo
+        {
+            get
+            {
+                return location.Y;
+            }
+            set
+            {
+                location.Y = value;
+            }
+        }
+
+        public int BInfo
+        {
+            get
+            {
+                return location.X;
+            }
+            set
+            {
+                location.X = value;
+            }
+        }
+
+        private bool[,] shelves;
+
+        public bool[,] Shelves
+        {
+            get
+            {
+                return shelves;
+            }
+            set
+            {
+                shelves = value;
+            }
+        }
+
+        public StorageUnit(Coordinate location, bool[,] shelves)
+        {
+            this.location = location;
+            this.shelves = shelves;
+        }
+
+        public StorageUnit(Coordinate location, int S){
+            this.location = location;
+            this.shelves = new bool[2,S];
+        }
+
+        public void fillShelf(int cInfo, int dInfo){
+            this.shelves[cInfo, dInfo - 1] = true;
+        }
+
+        public void emptyShelf(int cInfo, int dInfo)
+        {
+            this.shelves[cInfo, dInfo - 1] = false;
+        }
+    }
+}
